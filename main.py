786be4dce91948ae7a056e6bbd0f2c086243f5c1
Chauncey <chaunceyplum@gmail.com>
2024-03-10 20:02:58 -0400

    api issue fixed, now fixing single player info

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from nba_api.stats.endpoints import playercareerstats
 from nba_api.stats.endpoints import playerindex
 from nba_api.stats.endpoints import playergamelog
-
+from nba_api.stats.endpoints import commonplayerinfo
 from nba_api.stats.static.players import get_active_players
 from nba_api.stats.static.players import get_players
-
+from nba_api.stats.static import players
 from typing import Union
 import json
 from fastapi import FastAPI
+from datetime import datetime, timedelta
 
 app = FastAPI()
 
@@ -27,5 +28,14 @@ def get_active_players():
 
 @app.get("/get_player/{player_id}")
 def get_player(player_id:int):
-    player_info = playercareerstats.PlayerCareerStats(player_id=player_id)
-    return player_info.get_dict()
+    player_info = commonplayerinfo.CommonPlayerInfo(player_id=player_id)
+
+    data = {"data":player_info}
+    return data.get_dict()
+    # player = players.find_player_by_id(player_id=player_id)
+    # today = datetime.now()
+    # today += timedelta(days=5)
+    # iso_date = today.isoformat()
+    # player_info =  playergamelog.PlayerGameLog(player_id=player_id,season='2023-2024',season_type_all_star='Regular Season', date_from_nullable=iso_date)
+    # return player_info.get_dict()
+    # return player
